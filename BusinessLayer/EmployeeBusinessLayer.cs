913f2f15d5bc2c6a03020e360254813e6b28e347
muswilam <jackswilam@gmail.com>
2019-05-01 13:58:58 +0200

    Add logic to view list of Employees

--- BusinessLayer/EmployeeBusinessLayer.cs
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace BusinessLayer
 {
     public class EmployeeBusinessLayer
     {
+        public IEnumerable<Employee> GetAllEmployees
+        {
+            get
+            {
+                List<Employee> employees = new List<Employee>();
+                string cs = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.Id = (Int32)rdr["Id"];
+                        employee.Name = (string)rdr["Name"];
+                        employee.Gender = (string)rdr["Gender"];
+                        employee.City = (string)rdr["City"];
+                        employee.DateOfBirth = (DateTime)rdr["DateOfBirth"];
+
+                        employees.Add(employee);
+                    }
+                }
+                return employees;
+            }
+        }
     }
 }
---
+++ CRUD-BusinessLayer/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace CRUD_BusinessLayer.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        EmployeeBusinessLayer db = new EmployeeBusinessLayer();
+        public ActionResult List()
+        {
+            return View(db.GetAllEmployees.ToList());
+        }
+    }
+}
